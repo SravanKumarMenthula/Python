a89eb06b73f29241512557186949d7d0425bb5af
Sravan Kumar <33430132+SravanKumarMenthula@users.noreply.github.com>
2018-02-27 23:06:09 +0530

    Change in the name parameter

--- Keyword_arguments.py
+++ Keyword_arguments.py
@@ -2,6 +2,6 @@ def key_word_arg(name = 'sravan', interest = 'likes', food = 'biryani'):
   print(name, interest, food)
 
 key_word_arg()
-key_word_arg('anusha','likes','sweets')
+key_word_arg('X','likes','sweets')
 key_word_arg(name = 'kohli', food = 'green tea')
 key_word_arg(name = 'dhoni')
